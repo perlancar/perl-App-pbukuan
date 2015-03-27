f79f4f2f126e8180faa66640506ff29449091792
perlancar (@pc-office) <perlancar@gmail.com>
2015-03-27 19:06:24 +0700

    Initial commit

---
+++ lib/App/pbukuan.pm
@@ -0,0 +1,28 @@
+package App::pbukuan;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+1;
+# ABSTRACT: Collection of CLI utilities for accounting/finance/banking
+
+=head1 SYNOPSIS
+
+This is the first iteration of collection of
+accounting-/finance-/banking-related tools and libraries which I write. They
+will eventually be reorganized further as things get into clearer shapes.
+
+
+=head1 SEE ALSO
+
+L<Finance::Bank::ID::BCA>
+
+L<Finance::Bank::ID::Mandiri>
+
+L<Ledger::Parser>
+
+=cut
